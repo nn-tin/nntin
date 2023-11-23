d27d4652925b7f4c3f469f5c97ae93d503766f31
nn-tin <nguyenngoctin53@gmail.com>
2023-11-24 01:47:18 +0700

    First change

---
+++ bt1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+std:: string ten; int namsinh;
+
+int main() {
+
+    std:: cout << "Ten = ";
+    std:: getline(std:: cin, ten);
+    std:: cout << "Nam sinh = ";
+    std:: cin >> namsinh;
+    if (namsinh<2023) {std:: cout << "Chao ban " << ten << ", nam nay ban " << 2023 - namsinh << " tuoi.";}
+    else std:: cout << "Khong hop le";
+}
